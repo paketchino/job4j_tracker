a090cf6defcfe2c6a79e5642b4858e3e64fc8a06
paketchino <chansgorn@gmail.com>
2020-12-02 23:04:19 +0300

    Взаимодействие между обьектов

---
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+import ru.job4j.oop.Ball;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+        hare.tryEat(ball);
+        wolf.tryEat(ball);
+        fox.Eat(ball);
+    }
+}
