4c91cb2ba32881725b97984a7a26bc750ff859f5
paketchino <chansgorn@gmail.com>
2020-12-06 12:16:59 +0300

    Поиск максимального числа

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -1,24 +1,25 @@
 package ru.job4j.oop;
 
 public class Max {
-    public double max(double first, double second, double third) {
-        double result = (first > second ? first : second);
-        return result > third ? result : third;
+    public double max(double first, double second) {
+        return first > second ? first : second;
     }
-
+    public double max(double first, double second, double third){
+        return max(first,
+                max(second,third)
+        );
+    }
     public double max(double first, double second, double third, double fourth) {
-        double rsl = (Math.max(first, second));
-        double result = rsl > third ? rsl : third;
-        double total = Math.max(result, fourth);
-        return total;
+        return  max(first,
+                max(second, third, fourth)
+        );
     }
-
     public static void main(String[] args) {
         Max maxToFirst = new Max();
         Max maxToThird = new Max();
         double a = maxToFirst.max(5, 3,2);
         System.out.println(a);
-        double b = maxToThird.max(1, 2, 6, 2);
+        double b = maxToThird.max(1, 3,   8, 2);
         System.out.println(b);
     }
 }
