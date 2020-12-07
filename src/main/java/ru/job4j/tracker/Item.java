adf2187a59924b69d1197cda4cd1f08d2e157082
paketchino <chansgorn@gmail.com>
2020-12-07 23:05:39 +0300

    2. Перегрузка конструктора

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,18 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
+
+    public Item() {
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Item(String name) {
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public Item(String id) {
     }
 }
