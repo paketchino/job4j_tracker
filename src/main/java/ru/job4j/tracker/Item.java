99f98cff3a38fdd7113644b10e80f538a7bd98d6
paketchino <chansgorn@gmail.com>
2020-12-11 20:32:00 +0300

    7. toString

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,38 +3,28 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 
 public class Item {
+
     private int id;
     private String name;
 
-
-    public Item() {
-        System.out.println();
-    }
-
     public Item(String name) {
        this.name = name;
-
     }
     public Item(int id) {
       this.id = id;
     }
-
-    public int getId() {
-        return id;
-    }
-
     public void setId(int id) {
         this.id = id;
     }
-
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
-
+    public String getName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
     @Override
     public String toString() {
         return "Item{" +
