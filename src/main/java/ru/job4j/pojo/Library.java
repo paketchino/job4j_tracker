3c688873051ece32aab367553a2a9693883a598f
paketchino <chansgorn@gmail.com>
2020-12-11 21:00:15 +0300

    3. Массивы и модели

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,13 +2,13 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book clean_code = new Book("Clean code", 350);
-        Book java_program = new Book("Java Programing", 500);
+        Book cleanCode = new Book("Clean code", 350);
+        Book javaProgram = new Book("Java Programing", 500);
         Book grammar = new Book("Grammar", 800);
-        Book kotlin = new Book("kotlin", 600);
+        Book kotlin = new Book("Kotlin", 600);
         Book[] books = new Book[4];
-        books[0] = clean_code;
-        books[1] = java_program;
+        books[0] = cleanCode;
+        books[1] = javaProgram;
         books[2] = grammar;
         books[3] = kotlin;
         for (int i = 0; i < books.length; i++) {
@@ -16,14 +16,14 @@ public class Library {
             System.out.println(bk.getName() + " " + bk.getNumberOfPages());
         }
         books[0] = kotlin;
-        books[3] = clean_code;
+        books[3] = cleanCode;
         for (int i = 0; i < books.length; i++) {
             Book bk = books[i];
             System.out.println(bk.getName() + " " + bk.getNumberOfPages());
         }
         for (int i = 0; i < books.length; i++) {
             Book bk = books[i];
-            if ("clean_code".equals(books[i])) {
+            if ("Clean code".equals(books[i])) {
                 System.out.println(bk.getName() + " " + bk.getNumberOfPages());
             }
         }
