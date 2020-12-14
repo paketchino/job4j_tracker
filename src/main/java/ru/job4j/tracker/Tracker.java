bb1fa9b01455123fa2567663b51e4e975a545964
paketchino <chansgorn@gmail.com>
2020-12-14 23:52:25 +0300

    5. Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,25 +1,40 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+
     public Item add(Item item) {
+        /**
+         * добавляет заявку в массив items
+         * через setId проставляется ключ
+         * ids используется для регенерации нового ключа
+         */
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
+    /**
+     * size - поле массива
+     *
+     * @return
+     */
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                // Не понимаю что тут писать
             }
         }
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
+
 }
