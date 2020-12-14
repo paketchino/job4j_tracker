35b0bd5e9db432b90d6a7b39f515ae3df158f786
paketchino <chansgorn@gmail.com>
2020-12-14 23:58:54 +0300

    6. Date. Отображение даты

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,9 +1,16 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item(2);
-        System.out.println(item.toString());
+        Item item = new Item(LocalDateTime.now());
+        item.getCreated();
 
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDataTimeFormat = localDateTime.format(formatter);
+        System.out.println(currentDataTimeFormat);
     }
 }
