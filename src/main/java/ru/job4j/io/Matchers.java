5224d63912adf7f5013662a32d29f31cbcbc02f1
paketchino <chansgorn@gmail.com>
2020-12-23 21:15:09 +0300

    2. Scanner и чтение числа из консоли.

--- src/main/java/ru/job4j/io/Matchers.java
+++ src/main/java/ru/job4j/io/Matchers.java
@@ -4,26 +4,23 @@ import java.util.Random;
 import java.util.Scanner;
 
  public class Matchers {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int matchers = 11;
-        boolean rsl = matchers > 1;
-        while (rsl) {
-            System.out.println("Количество спичек: " + matchers);
-            int firstPlayer = Integer.valueOf(input.nextLine());
-            matchers =- firstPlayer;
-            System.out.println("Matchers left" + matchers);
-            if (matchers < 1) {
-                System.out.println("First player win" + firstPlayer);
-                break;
-            }
-            int secondPlayer = Integer.valueOf(input.nextLine());
-            matchers =- secondPlayer;
-            System.out.println("Matchers left " + matchers);
-            if (matchers < 1) {
-                System.out.println("Second player win " + secondPlayer);
-                break;
-            }
-        }
-    }
-}
+     public static void main(String[] args) {
+         boolean player = true;
+         Scanner input = new Scanner(System.in);
+         int matches = 11;
+         System.out.println("Добро пожаловать в игру " + matches + " спичек");
+         while (matches > 0) {
+             String name = player ? "Player First" : "Player Second";
+             System.out.println(name + " выберите количество спичек от 1 до 3");
+             int count = Integer.valueOf(input.nextLine());
+             matches -= count;
+             if (matches > 0) {
+                 player = !player;
+                 System.out.println("На столе осталось " + matches);
+             }
+             if (matches == 0) {
+                 System.out.println("Выйграл игрок " + name);
+             }
+         }
+     }
+ }
