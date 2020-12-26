24342e3f05ea5b9e878cfb521f4578eb8bd672d2
paketchino <chansgorn@gmail.com>
2020-12-27 00:13:47 +0300

    3.1. Приведение типов. Повышение типа и понижение типа.

---
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class HierarchyUsage {
+    public static void main(String[] args) {
+    Freshman freshman = new Freshman();
+    Student student = freshman;
+    Object obj = freshman;
+
+    Object oFreshman = new Freshman();
+    Freshman freshmanFromObject = (Freshman) oFreshman;
+
+    }
+}
--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,19 +1,4 @@
 package ru.job4j.oop;
 
-public class Student {
-    public void song() {
-        System.out.println("I believe I can fly");
-    }
-    public void music() {
-        System.out.println("tra tra tra");
-    }
-    public static void main(String[] args) {
-        Student Petya = new Student();
-        Petya.music();
-        Petya.music();
-        Petya.music();
-        Petya.song();
-        Petya.song();
-        Petya.song();
-    }
+public class Student extends Transport {
 }
