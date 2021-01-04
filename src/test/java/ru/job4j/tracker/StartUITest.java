326f4ebb8c68706fa71ca042df789a8fdf4135a2
paketchino <chansgorn@gmail.com>
2021-01-04 13:43:28 +0300

    9.2. Тесты вывода на консоль

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Test;
 import java.util.Scanner;
 
+import static org.hamcrest.Matchers.sameInstance;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.nullValue;
@@ -11,47 +12,92 @@ public class StartUITest {
 
     @Test
     public void whenAddItem() {
+        Output output = new ConsoleOutput();
         Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
+                new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
-                new Exit()
+                new CreateAction(output),
+                new Exit(output)
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(output).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
 
     @Test
     public void whenNeedDeleteItem() {
+        Output output = new ConsoleOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted Item"));
         Input in = new StubInput(
                 new String[]{"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
-                new DeleteAction(),
-                new Exit()
+                new DeleteAction(output),
+                new Exit(output)
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(output).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
+
     @Test
     public void whenNeedReplaceItem() {
+        Output output = new ConsoleOutput();
         Tracker tracker = new Tracker();
         /* Добавим в tracker новую заявку */
         Item item = tracker.add(new Item("Replaced Item"));
         /* Входные данные должны содержать ID добавленной заявки item.getId() */
         String replaceName = "New item name";
         Input in = new StubInput(
-                new String[]{"0",String.valueOf(item.getId()),"New item name", "1"}
+                new String[]{"0", String.valueOf(item.getId()), "New item name", "1"}
         );
         UserAction[] actions = {
-                new ReplaceAction(),
-                new Exit()
+                new ReplaceAction(output),
+                new Exit(output)
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(output).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName(), is(replaceName));
+    }
+
+    @Test
+    public void WhenExit() {
+        Output output = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit(output)
+        };
+        new StartUI(output).init(in, tracker, actions);
+        assertThat(output.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0 Exit" + System.lineSeparator()
+        ));
+    }
+
+    @Test
+    public void WhenFindAll() {
+        Output output = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"0", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new FindIdAction(output),
+                new Exit(output)
+        };
+        new StartUI(output).init(in, tracker, actions);
+        assertThat(output.toString(), is(
+                "Menu." + System.lineSeparator() +
+                "1. Find All Action" + System.lineSeparator() +
+                 "2. CreateAction " + System.lineSeparator() +
+                 "3. ReplaceAction " + System.lineSeparator() +
+                 "4. Delete Action " + System.lineSeparator() +
+                 "5. Find Id Action " + System.lineSeparator() +
+                 "6. FindNameAction "  + System.lineSeparator() +
+                 "0. Exit " + System.lineSeparator()
+        ));
     }
 }
