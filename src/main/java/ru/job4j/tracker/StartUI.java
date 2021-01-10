0847334e550de8699c5fc5210100e15c616e09e6
paketchino <chansgorn@gmail.com>
2021-01-10 12:52:32 +0300

    9.2. Тесты вывода на консоль

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,21 +9,13 @@ public class StartUI {
     }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
-        try {
             boolean run = true;
             while (run) {
                 this.showMenu(actions);
                 int select = input.askInt("Select: ");
-                if (select < 0 || select <= actions.length) {
-                    out.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
-                    continue;
-                }
                 UserAction action = actions[select];
                 run = action.execute(input, tracker);
             }
-        } catch (ArrayIndexOutOfBoundsException abe) {
-            System.out.println("Wrong input enter number 0 to 6");
-        }
     }
 
     private void showMenu(UserAction[] actions) {
@@ -34,9 +26,8 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        try {
             Output output = new ConsoleOutput();
-            Input input = new ValidateInput(output, new ConsoleInput());
+            Input input = new ConsoleInput();
             Tracker tracker = new Tracker();
             UserAction[] actions = {
                     new CreateAction(output),
@@ -48,11 +39,6 @@ public class StartUI {
                     new Exit(output)
             };
             new StartUI(output).init(input, tracker, actions);
-        } catch (NumberFormatException nfe) {
-            nfe.printStackTrace();
-        } catch (ArrayIndexOutOfBoundsException abe) {
-            abe.printStackTrace();
-        }
     }
 }
 
