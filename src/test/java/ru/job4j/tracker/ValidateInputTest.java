ea1a25922f1dd6bdec1eb9311ec21da5f77678ec
paketchino <chansgorn@gmail.com>
2021-01-15 23:52:34 +0300

    2.1. Тест на ValidateInput

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class ValidateInputTest {
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected, is(1));
+    }
+    @Test
+    public void whenInvalidMultiple() {
+        Output output = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "1","1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenInvalidNegative(){
+        Output output = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu :");
+        assertThat(selected, is(-1));
+    }
+}
