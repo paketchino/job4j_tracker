b265ea443fb7ac786f2758e4117c4fd3d1ca0482
paketchino <chansgorn@gmail.com>
2021-02-25 21:46:37 +0300

    3. Компаратор для строк [#380464]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,6 +5,15 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        return Integer.compare(left.length(), right.length());
+        int rsl = Integer.compare(left.length(), right.length());
+        int j = 0;
+        for (int i = 0; i < left.length(); i++) {
+                if (left.charAt(i) != right.charAt(i)) {
+                    rsl = Character.compare(left.charAt(i), right.charAt(i));
+                    break;
+                }
+                j++;
+            }
+        return rsl;
     }
 }
--- src/test/java/ru/job4j/collection/StringCompareTest.java
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.collection;
 
+import ru.job4j.collection.StringCompare;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNull;
@@ -7,7 +8,6 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.greaterThan;
-
 
 public class StringCompareTest {
 
@@ -20,8 +20,9 @@ public class StringCompareTest {
         );
         assertThat(rsl, is(0));
     }
+
     @Test
-    public void whenLeftLessThanRightResultShouldBeNegative () {
+    public void whenLeftLessThanRightResultShouldBeNegative() {
         StringCompare compare = new StringCompare();
         int rst = compare.compare(
                 "Ivanov",
@@ -31,7 +32,7 @@ public class StringCompareTest {
     }
 
     @Test
-    public void whenLeftGreaterThanRightResultShouldBePositive () {
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
         StringCompare compare = new StringCompare();
         int rst = compare.compare(
                 "Petrov",
@@ -39,4 +40,45 @@ public class StringCompareTest {
         );
         assertThat(rst, greaterThan(0));
     }
+
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Patrova",
+                 "Petrov"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenSecondCompareWithEmptyLine() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                " ",
+                "Petrov"
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenFirstCompareWithEmptyLineShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Petrov",
+                " "
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
 }
