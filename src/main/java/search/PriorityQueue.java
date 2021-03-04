c79d6a6c8408a06fe3ea95fcb161dd4c5b74c762
paketchino <chansgorn@gmail.com>
2021-03-04 21:26:47 +0300

    4. Генерация документации по JavaDoc [#380458]

--- src/main/java/search/PriorityQueue.java
+++ src/main/java/search/PriorityQueue.java
@@ -2,14 +2,24 @@ package search;
 
 import java.util.LinkedList;
 
+/**
+ *  Класс описывает работу простейшей очереди по приоритету, которая работает
+ *  * по принципу FIFO (first in - first out)
+ *  * @author Roman Kulyanin
+ *  * @version 1.0
+ */
+
 public class PriorityQueue {
+    /**
+     * Хранение задания осуществляется в спике LinkedList
+     */
     private LinkedList<Task> tasks = new LinkedList<>();
 
     /**
-     * Метод должен вставлять в нужную позицию элемент.
-     * Позиция определяется по полю приоритет.
-     * Для вставки использовать add(int index, E value)
-     * @param task задача
+     * Метод принимает на вход заявку и добавляет ее в очередь.
+     * Если встречаются 2 задания с одинаковым приорететом, то в очереди
+     * они распределяются по принпицу Firs in - First Out
+     * @param task задача которая добавляется в очередь
      */
 
     public void put(Task task) {
@@ -22,6 +32,11 @@ public class PriorityQueue {
         }
         this.tasks.add(index, task);
     }
+
+    /**
+     *
+     * @return возвращает задачу из головы или null если очереб пуста
+     */
 
     public Task take() {
         return tasks.poll();
