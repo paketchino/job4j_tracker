da742321319c4012f41a7c7422b51bce1249d144
paketchino <chansgorn@gmail.com>
2021-03-08 15:56:24 +0300

    1. Экзамен. Коллекции. [#380462]

--- src/main/java/ru/job4j/samopoznanye/Article.java
+++ src/main/java/ru/job4j/samopoznanye/Article.java
@@ -1,22 +1,22 @@
 package ru.job4j.samopoznanye;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
         boolean rsl = true;
-        String[] originText = origin.split(" ");
-        String[] lineText = line.split(" ");
+        String or = origin.replaceAll("\\p{P}", "");
+        String ln = line.replaceAll("\\p{P}", "");
+        String[] originText = origin.split("");
+        String[] lineText = line.split("");
         Set<String> hashSet = new HashSet<>();
-        for (String o: originText) {
+        for (String o : originText) {
             hashSet.add(o);
         }
         for (String words : lineText) {
             if (!hashSet.contains(words)) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
