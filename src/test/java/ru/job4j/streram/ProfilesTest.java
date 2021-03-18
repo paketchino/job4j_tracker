7d3eb0c214bb76d5adcdbad0c2165432dc20dc8f
paketchino <chansgorn@gmail.com>
2021-03-18 22:10:52 +0300

    2. Список адресов [#380305]

--- src/test/java/ru/job4j/streram/ProfilesTest.java
+++ src/test/java/ru/job4j/streram/ProfilesTest.java
@@ -10,14 +10,16 @@ import static org.hamcrest.core.Is.is;
 public class ProfilesTest {
     @Test
     public void whenCreateList() {
-        List<Address> addresses = List.of(
-                new Address("Penza", "ul.DDDDD", 5, 3),
-                new Address("Moscow", "ul.323212", 5, 3),
-                new Address("Sankt-Peterburg", "ul.454454", 5, 3),
-                new Address("DNO", "ul.544512", 5, 3),
-                new Address("Russiz", "ul.98798", 5, 3)
+        List<Profile> addresses = List.of(
+                new Profile(new Address("Pebza", "Kolokolov", 3, 5)),
+                new Profile(new Address("Moscow", "Brigadirov", 90, 32)),
+                new Profile(new Address("Zarechnya", "Zelenya", 99, 23))
         );
-        Profile excepted = new Profile(   new Address("Moscow", "ul.323212", 5, 3));
+        Profiles profiles = new Profiles();
+        List<Profile> excepted = new ArrayList<>();
+        excepted.add(new Profile(new Address("Pebza", "Kolokolov", 3, 5)));
+        excepted.add(new Profile(new Address("Moscow", "Brigadirov", 90, 32)));
+        excepted.add(new Profile(new Address("Zarechnya", "Zelenya", 99, 23)));
         assertThat(addresses, is(excepted));
     }
 }
