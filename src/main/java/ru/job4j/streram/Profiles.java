7dde71bf7a556e9fbf43f9fe7270dcc63c315480
paketchino <chansgorn@gmail.com>
2021-03-20 00:05:00 +0300

    3. Уникальность элементов и сортировка. [#380306]

--- src/main/java/ru/job4j/streram/Profiles.java
+++ src/main/java/ru/job4j/streram/Profiles.java
@@ -1,12 +1,16 @@
 package ru.job4j.streram;
 
+import java.util.Comparator;
 import java.util.stream.*;
 import java.util.List;
 
 public class Profiles {
     List<Address> collect(List<Profile> profiles) {
         return profiles
-                .stream().map(Profile::getAddress)
+                .stream()
+                .map(Profile::getAddress)
+                .distinct()
+                .sorted()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/streram/ProfilesTest.java
+++ src/test/java/ru/job4j/streram/ProfilesTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.streram;
 
 import org.junit.Test;
-
 import java.util.List;
 import java.util.ArrayList;
 import static org.junit.Assert.assertThat;
@@ -10,16 +9,17 @@ import static org.hamcrest.core.Is.is;
 public class ProfilesTest {
     @Test
     public void whenCreateList() {
-        List<Profile> addresses = List.of(
-                new Profile(new Address("Pebza", "Kolokolov", 3, 5)),
-                new Profile(new Address("Moscow", "Brigadirov", 90, 32)),
-                new Profile(new Address("Zarechnya", "Zelenya", 99, 23))
-        );
-        Profiles profiles = new Profiles();
-        List<Profile> excepted = new ArrayList<>();
-        excepted.add(new Profile(new Address("Pebza", "Kolokolov", 3, 5)));
-        excepted.add(new Profile(new Address("Moscow", "Brigadirov", 90, 32)));
-        excepted.add(new Profile(new Address("Zarechnya", "Zelenya", 99, 23)));
-        assertThat(addresses, is(excepted));
+        Profiles profile = new Profiles();
+        List<Address> rsl = profile.collect(List.of(
+                new Profile(new Address("Penza", "Zalenya", 10, 5)),
+                new Profile(new Address("Moscow", "Talay", 445, 23)),
+                new Profile(new Address("Sankt-Peterburg", "Konstatina", 323,2))
+        ));
+        List<Address> excepted = profile.collect(List.of(
+                new Profile(new Address("Penza", "Zalenya", 10, 5)),
+                new Profile(new Address("Moscow", "Talay", 445, 23)),
+                new Profile(new Address("Sankt-Peterburg", "Konstatina", 323,2))
+        ));
+        assertThat(rsl, is(excepted));
     }
 }
