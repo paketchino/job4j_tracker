5b66fbc201b8bad40933bbbeb41cb2c8b5a1ebee
paketchino <chansgorn@gmail.com>
2021-03-23 11:27:39 +0300

    0. Stream API улучшения [#380314]

--- src/test/java/ru/job4j/streram/StudentLevelTest.java
+++ src/test/java/ru/job4j/streram/StudentLevelTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.streram;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -19,6 +20,24 @@ public class StudentLevelTest {
                 new Student(20, "Masha"),
                 new Student(128, "Pety")
         );
+        assertThat(StudentLevel.levelOf(input, 19), is(excepted));
+    }
+
+    @Test
+    public void whenOnlyNull() {
+        List<Student> input = new ArrayList<>();
+        input.add(null);
+        List<Student> excepted = List.of();
         assertThat(StudentLevel.levelOf(input, 20), is(excepted));
     }
+
+    @Test
+    public void whenHasNull() {
+        List<Student> input = new ArrayList<>();
+        input.add(null);
+        input.add(new Student(60, "Roman"));
+        List<Student> excepted = List.of(new Student(60, "Roman"));
+        assertThat(StudentLevel.levelOf(input, 59), is(excepted));
+    }
+
 }
