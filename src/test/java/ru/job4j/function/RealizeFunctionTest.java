c10ccf8f19ed287ae6c50ba494d07bda8083f04f
paketchino <chansgorn@gmail.com>
2021-03-23 23:00:11 +0300

    5. Подсчет функции в диапазоне. [#380289]

--- src/test/java/ru/job4j/function/RealizeFunctionTest.java
+++ src/test/java/ru/job4j/function/RealizeFunctionTest.java
@@ -13,21 +13,21 @@ public class RealizeFunctionTest {
     @Test
     public void whenLinearFunctionThenLinearResults() {
         List<Double> result = RealizeFunction.diapason(5, 8, x -> 2 * x + 1);
-        List<Double> excepted = Arrays.asList(11D, 13D, 15D);
+        List<Double> excepted = List.of(11D, 13D, 15D);
         assertThat(result, is(excepted));
     }
 
     @Test
     public void whenSquareFunctionThenSquareResult() {
         List<Double> result = RealizeFunction.diapason(5, 8, x -> x * x);
-        List<Double> excepted = Arrays.asList(25D, 36D, 49D);
+        List<Double> excepted = List.of(25D, 36D, 49D);
         assertThat(result, is(excepted));
     }
 
     @Test
     public void whenIndicative() {
         List<Double> result = RealizeFunction.diapason(5, 8, x -> Math.pow(x, 3));
-        List<Double> excepted = Arrays.asList(125D, 216D, 343D);
+        List<Double> excepted = List.of(125D, 216D, 343D);
         assertThat(result, is(excepted));
     }
 }
